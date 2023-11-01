61f836138f346f1d94155c3bb4f141eeb73a2d86
IvanM <ivan.montes@outlook.com>
2023-11-01 10:07:18 +0100

    Develop methods

--- src/main/java/ime/flixing/dao/FlixDao.java
+++ src/main/java/ime/flixing/dao/FlixDao.java
@@ -1,5 +1,15 @@
 package ime.flixing.dao;
 
+import java.util.List;
+
+import ime.flixing.entity.Flix;
+
 public interface FlixDao {
 
+	List<Flix>getAllFlix();
+	Flix getFlixById(Long id);
+	Flix saveFlix(Flix flix);
+	Flix updateFlix(Long id, Flix flix);
+	void deleteFlix(Long id);
+	
 }
