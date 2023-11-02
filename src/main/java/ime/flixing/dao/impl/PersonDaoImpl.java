04ed6e85f3054584585a37ea8ab691d548a28ec6
IvanM <ivan.montes@outlook.com>
2023-11-02 16:41:32 +0100

    Develop methods

--- src/main/java/ime/flixing/dao/impl/PersonDaoImpl.java
+++ src/main/java/ime/flixing/dao/impl/PersonDaoImpl.java
@@ -1,7 +1,66 @@
 package ime.flixing.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 import ime.flixing.dao.PersonDao;
+import ime.flixing.entity.Person;
+import ime.flixing.util.HibernateUtil;
 
 public class PersonDaoImpl implements PersonDao{
+
+	@Override
+	public List<Person> getAllPerson() {
+		Session session = HibernateUtil.getSession().openSession();
+		Query<Person> query = session.createQuery("FROM Person", Person.class);
+		List<Person>list = query.list();
+		session.close();
+		return list;
+	}
+
+	@Override
+	public Person getPersonById(Long id) {
+		Session session = HibernateUtil.getSession().openSession();
+		Person personFound = session.get(Person.class, id);
+		session.close();
+		return personFound;
+	}
+
+	@Override
+	public Person savePerson(Person person) {
+		Session session = HibernateUtil.getSession().openSession();
+		session.beginTransaction();
+		session.persist(person);
+		session.getTransaction().commit();
+		Person personFound = session.get(Person.class, person.getPersonId());
+		session.close();
+		return personFound;
+	}
+
+	@Override
+	public Person updatePerson(Long id, Person person) {
+		Session session = HibernateUtil.getSession().openSession();
+		session.beginTransaction();
+		Person personedo = session.get(Person.class, id);
+		personedo.setName(person.getName());
+		personedo.setSurname(person.getSurname());
+		session.persist(personedo);
+		session.getTransaction().commit();
+		Person personFound = session.get(Person.class, id);
+		session.close();
+		return personFound;
+	}
+
+	@Override
+	public void deletePerson(Long id) {
+		Session session = HibernateUtil.getSession().openSession();
+		session.beginTransaction();
+		Person personFound = (Person) session.get(Person.class, id);
+		session.remove(personFound);
+		session.getTransaction().commit();
+        session.close();
+	}
 
 }
