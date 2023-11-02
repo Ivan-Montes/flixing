8cd8d708b5c98a3b7cc1a08ba8fa99728cbb3f35
IvanM <ivan.montes@outlook.com>
2023-11-02 16:41:13 +0100

    Complete interface

--- src/main/java/ime/flixing/dao/PersonDao.java
+++ src/main/java/ime/flixing/dao/PersonDao.java
@@ -1,5 +1,14 @@
 package ime.flixing.dao;
 
+import java.util.List;
+import ime.flixing.entity.Person;
+
 public interface PersonDao {
 
+	List<Person>getAllPerson();
+	Person getPersonById(Long id);
+	Person savePerson(Person person);
+	Person updatePerson(Long id, Person person);
+	void deletePerson(Long id);
+	
 }
