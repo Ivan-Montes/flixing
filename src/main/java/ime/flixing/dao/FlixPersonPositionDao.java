9b4f0ec2b667d81b3a969950612d757df255e580
IvanM <ivan.montes@outlook.com>
2023-11-03 16:24:54 +0100

    Create interface

--- src/main/java/ime/flixing/dao/FlixPersonPositionDao.java
+++ src/main/java/ime/flixing/dao/FlixPersonPositionDao.java
@@ -1,5 +1,10 @@
 package ime.flixing.dao;
 
+import ime.flixing.entity.FlixPersonPosition;
+
 public interface FlixPersonPositionDao {
 
+	FlixPersonPosition saveFlixPersonPosition(Long flixId, Long personId, Long positionId);
+	void deleteFlixPersonPosition(Long flixId, Long personId, Long positionId);
+	
 }
