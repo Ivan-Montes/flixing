0c503f3f251e453019fd83e7674fdea581a4f3f7
IvanM <ivan.montes@outlook.com>
2023-11-23 13:42:01 +0100

    Create new method

--- src/main/java/ime/flixing/dao/PositionDao.java
+++ src/main/java/ime/flixing/dao/PositionDao.java
@@ -6,11 +6,12 @@ import ime.flixing.entity.Position;
 
 public interface PositionDao {
 
-	List<Position>getAllPosition();
+	List<Position> getAllPosition();
 	Position getPositionById(Long id);
 	Position getPositionByIdEagger(Long id);
 	Position savePosition(Position position);
 	Position updatePosition(Long id, Position position);
 	void deletePosition(Long id);
+	List<Position> getPositionByNameId(String name);
 	
 }
