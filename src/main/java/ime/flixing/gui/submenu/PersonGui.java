b4cccf4151006177da1beda619bf628d6df4929b
IvanM <ivan.montes@outlook.com>
2023-12-03 11:01:00 +0100

    Complete class

--- src/main/java/ime/flixing/gui/submenu/PersonGui.java
+++ src/main/java/ime/flixing/gui/submenu/PersonGui.java
@@ -1,7 +1,13 @@
 package ime.flixing.gui.submenu;
 
+import ime.flixing.gui.Checker;
 import ime.flixing.gui.DecoHelper;
 import ime.flixing.gui.Prompter;
+import ime.flixing.dao.*;
+import ime.flixing.dao.impl.*;
+import ime.flixing.entity.Person;
+
+import java.util.List;
 
 public class PersonGui {
 
@@ -75,23 +81,117 @@ public void init() {
 	}
 	
 	private void getAllPersonOption() {
-		
+		PersonDao personDao = new PersonDaoImpl();
+		List<Person>list = personDao.getAllPerson();
+		list.forEach(System.out::println);
 	}
 	
 	private void getPersonByIdOption() {
 		
+		String cod = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_COD);
+
+		if ( Checker.checkDigits(cod) ) {
+			
+			PersonDao personDao = new PersonDaoImpl();
+			Person person = personDao.getPersonById(Long.parseLong(cod));
+			
+			if ( person != null) {				
+				
+				System.out.println(person);
+
+			}else {
+				System.out.println("\t" + DecoHelper.MSG_NULL_ERROR);
+			}
+			
+		}else {			
+			System.out.println("\t" + DecoHelper.MSG_COD_ERROR);			
+		}
 	}
 	
 	private void savePersonOption() {
 		
+		String name = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_NAME);
+		String surname = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_SURNAME);
+		
+		if ( Checker.checkName(name) && Checker.checkSurname(surname) ) {
+			
+			PersonDao personDao = new PersonDaoImpl();
+			Person person = new Person();
+			person.setName(name);
+			person.setSurname(surname);
+			Person personSaved = personDao.savePerson(person);
+			System.out.println(personSaved);
+			
+		}else {			
+			System.out.println("\t" + DecoHelper.MSG_DATA_ERROR);
+		}	
 	}
 	
 	private void updatePersonOption() {
 		
+		String personCod = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_COD);
 		
+		if ( Checker.checkDigits(personCod) ) {
+			PersonDao personDao = new PersonDaoImpl();
+			Person personFound = personDao.getPersonById(Long.parseLong(personCod));
+			
+			if ( personFound != null ) {
+				
+				System.out.println(personFound);
+				String name = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_NAME);
+				String surname = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_SURNAME);
+				
+				if ( Checker.checkName(name) && Checker.checkSurname(surname) ) {
+					
+					Person person = new Person();
+					person.setName(name);
+					person.setSurname(surname);
+					Person personSaved = personDao.updatePerson(Long.parseLong(personCod), person);
+					System.out.println(personSaved);
+
+				}else {					
+					System.out.println("\t" + DecoHelper.MSG_DATA_ERROR);
+				}
+				
+			}else {
+				System.out.println("\t" + DecoHelper.MSG_NULL_ERROR);
+			}	
+			
+		}else {
+			System.out.println("\t" + DecoHelper.MSG_COD_ERROR);
+		}	
+
 	}
 	
 	private void deletePersonOption() {
 		
+		String personCod = Prompter.readOptWithMsg(DecoHelper.MSG_WRITE_COD);
+		
+		if ( Checker.checkDigits(personCod) ) {
+			
+			PersonDao personDao = new PersonDaoImpl();
+			Person personFound = personDao.getPersonByIdEagger(Long.parseLong(personCod));
+			
+			if ( personFound != null ) {
+				
+				System.out.println(personFound);
+				
+				if ( Checker.checkConfirmation( Prompter.askConfirmation() ) ) {
+					
+						if ( personFound.getFlixPersonPosition().isEmpty() ) {
+						
+							personDao.deletePerson( Long.parseLong(personCod) );
+							System.out.println(DecoHelper.MSG_SUCCESSFULLY);
+						
+						}else {							
+							System.out.println("\t" + DecoHelper.MSG_ERROR_DELETE_ASSOCIATED_ITEMS);						
+						}
+				}				
+			}else {				
+				System.out.println("\t" + DecoHelper.MSG_NULL_ERROR);				
+			}			
+		}else {			
+			System.out.println("\t" + DecoHelper.MSG_COD_ERROR);			
+		}
 	}
 }
