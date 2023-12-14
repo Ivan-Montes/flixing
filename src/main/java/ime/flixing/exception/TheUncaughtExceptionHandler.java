967ac0ffc9ce5ab413877dd4af2d49fce427ed27
IvanM <ivan.montes@outlook.com>
2023-12-14 19:22:29 +0100

    Refactor class

--- src/main/java/ime/flixing/exception/TheUncaughtExceptionHandler.java
+++ src/main/java/ime/flixing/exception/TheUncaughtExceptionHandler.java
@@ -1,47 +1,37 @@
 package ime.flixing.exception;
 
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ime.flixing.gui.DecoHelper;
 
 public class TheUncaughtExceptionHandler implements UncaughtExceptionHandler {
+
+	private final Logger logger = Logger.getLogger(getClass().getName());
 
 	@Override
     public void uncaughtException(Thread t, Throwable e) {
 		
-        if (e instanceof NullPointerException nullE) {
-            handleNullPointerException( nullE );
+		String msg = "";
+		
+        if (e instanceof NullPointerException ex) {
+        	msg = String.format(DecoHelper.EX_MSG_TEMPLATE, DecoHelper.EX_NULL, ex.getMessage());
             
-        } else if (e instanceof java.lang.IllegalStateException illegalE) {
-            handleIllegalStateException( illegalE );
+        } else if (e instanceof java.lang.IllegalStateException ex) {
+        	msg = String.format(DecoHelper.EX_MSG_TEMPLATE, DecoHelper.EX_ILLEGAL_STATE, ex.getMessage());
             
-        } else if (e instanceof org.hibernate.service.spi.ServiceException serviceE) {
-            handleIllegalServiceException( serviceE );
+        } else if (e instanceof org.hibernate.service.spi.ServiceException ex) {
+        	msg = String.format(DecoHelper.EX_MSG_TEMPLATE, DecoHelper.EX_ILLEGAL_SERVICE, ex.getMessage());
             
-        }else if (e instanceof org.hibernate.engine.jdbc.env.spi.JdbcEnvironment jdbcE) {
-            handleJdbcEnvironmentException( jdbcE );
+        }else if (e instanceof org.hibernate.engine.jdbc.env.spi.JdbcEnvironment ex) {
+        	msg = String.format(DecoHelper.EX_MSG_TEMPLATE, DecoHelper.EX_HIBERNATE_JDBC, ((Throwable) ex).getMessage());
         }
         else {
-            handleGenericException(e);
+        	msg = String.format(DecoHelper.EX_MSG_TEMPLATE, DecoHelper.EX_UNKNOWN, e.getMessage());
         }
-    }
-
-    private void handleNullPointerException(NullPointerException e) {
-        System.out.println("Se ha producido una excepción de puntero nulo: " + e.getMessage());
-    }
-
-    private void handleIllegalStateException(java.lang.IllegalStateException e) {
-        System.out.println("Se ha producido una excepción de java.lang.IllegalStateException: " + e.getMessage());
-    }
-
-    private void handleIllegalServiceException(org.hibernate.service.spi.ServiceException e) {
-        System.out.println("Se ha producido una excepción de org.hibernate.service.spi.ServiceException: " + e.getMessage());
-    }
-
-    private void handleJdbcEnvironmentException(org.hibernate.engine.jdbc.env.spi.JdbcEnvironment e) {
-        System.out.println("Se ha producido una excepción de org.hibernate.engine.jdbc.env.spi.JdbcEnvironment: ");
-    }
-
-    private void handleGenericException(Throwable e) {
-        System.out.println("Se ha producido una excepción desconocida: " + e.getMessage());
+        
+        logger.log(Level.SEVERE, msg );
     }
 
 }
